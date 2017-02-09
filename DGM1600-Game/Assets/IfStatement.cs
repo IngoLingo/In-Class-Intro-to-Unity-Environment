76d67b4fbcd5ed47de94f14008139a82c0743066
Nathan Ethington <10598429@my.uvu.edu>
2017-02-09 12:28:29 -0700

    If Statements

---
+++ DGM1600-Game/Assets/IfStatement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatement : MonoBehaviour {
+    private int A = 10;
+    private int B = 10;
+    private string fstPlayerName = "Link";
+    private string sndPlayerName = "Ingo";
+    private bool awake = false;
+    private float time = 0.0f;
+
+
+    // Use this for initialization
+    void Start () {
+        if (A == B)
+        {
+            print("A is equal to B");
+        }
+        if (A != B)
+        {
+            print("A is not equal to B");
+        }
+        if (A > B)
+        {
+            print("A is greater then B");
+        }
+        if (A < B)
+        {
+            print("A is less than B");
+        }
+        if (A >= B)
+        {
+            print("A is greater than or equal to B");
+        }
+        if (A <= B)
+        {
+            print("A is less than or equal to B");
+        }
+    }
+}
---
+++ DGM1600-Game/Assets/MyIfStatement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyIfStatement : MonoBehaviour {
+    private int maxTime = 240;
+    private string fstPlayerName = "LINK";
+    private string sndPlayerName = "INGO";
+    private bool fstAwake = false;
+    private bool sndAwake = false;
+    private float time = 0f;
+    private int days = 0;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (days == 7) // This makes the script not continue.
+        {
+            print("This assignment is now over!");
+            time = 0f;
+        }
+
+        if (time >= maxTime)
+        {// Reset the clock once you hit the max time
+            print("It is now midnight...");
+            time = 0f;
+            days++;
+            print("Day: " + days);
+        }
+
+
+        if (time == 50)
+        {// This is letting us know that player 1 is awake.
+            print(fstPlayerName + " is now awake!");
+            fstAwake = true;
+        }
+        if (time == 70)
+        {// This is letting us know that player 2 is awake.
+            print(sndPlayerName + " is now awake!");
+            sndAwake = true;
+        }
+
+        if (time == 100)
+        {// This is letting us know that player 1 is having breakfast.
+            print(fstPlayerName + " is having breakfast.");
+        }
+        if (time == 120)
+        {// This is letting us know that player 2 went to school.
+            print(sndPlayerName + " went to school.");
+        }
+
+
+        if (time == 170)
+        {// This is letting us know that player 1 is having dinner.
+            print(fstPlayerName + " is having dinner.");
+        }
+        if (time == 180)
+        {// This is letting us know that player 2 came home from school.
+            print(sndPlayerName + " came home from school.");
+        }
+
+
+        if (time == 200)
+        {// This is letting us know that player 1 is asleep.
+            print(fstPlayerName + " is now asleep!");
+            fstAwake = false;
+        }
+        if (time == 230)
+        {// This is letting us know that player 2 is asleep.
+            print(sndPlayerName + " is now asleep!");
+            sndAwake = false;
+        }
+
+        // Increases the time by 1 and prints it out
+        time += 1f;
+    }
+}
